3c0d3ad9e8ce99fcb077acd2cce05d459751bc16
dangphu2412 <dangphu241299@gmail.com>
2026-01-02 11:25:11 +0700

    chore: trigger release

--- service-common/src/index.ts
+++ service-common/src/index.ts
@@ -1,3 +1,9 @@
-export { HttpGatewayExceptionFilter } from './exception/gateway-exception-filter';
-export { RpcServiceExceptionFilter } from './exception/rpc-exception-filter';
-export { BusinessException } from './exception/business-exception';
+export { HttpGatewayExceptionFilter } from "./exception/gateway-exception-filter";
+export { RpcServiceExceptionFilter } from "./exception/rpc-exception-filter";
+export {
+  BusinessException,
+  ErrorTag,
+  ExistedException,
+  InvalidArgumentException,
+  NotFoundException,
+} from "./exception/business-exception";
